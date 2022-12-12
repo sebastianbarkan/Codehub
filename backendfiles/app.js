8bd2553374dead986ec8eb8e9776e815667b4e68
sebastianbarkan <86000650+sebastianbarkan@users.noreply.github.com>
2022-12-12 10:41:58 -0500

    update url cors

--- backendfiles/app.js
+++ backendfiles/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(
   cors({
     allowedHeaders: ["Content-Type", "Authorization"],
-    origin: "http://localhost:3000",
+    origin: "http://www.coderhub.link",
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
     credentials: true,
   })
